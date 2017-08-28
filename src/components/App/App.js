6dfc5b348044842a90df9800775e194036249166
Artem Samofalov <dex157@gmail.com>
2017-08-28 20:15:41 +0500

    add styled-components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,17 @@
 import React, {Component} from 'react'
 import {Layout, Menu, Icon} from 'antd'
+import styled from 'styled-components'
 import 'antd/dist/antd.css'
 
 const {Content, Footer, Sider} = Layout
+
+const StyledLayout = styled(Layout)`
+  height: 100%;
+`
+
+const StyledFooter = styled(Footer)`
+  textAlign: center
+`
 
 class App extends Component {
   state = {
@@ -15,7 +24,7 @@ class App extends Component {
 
   render() {
     return (
-      <Layout style={{height: '100%'}}>
+      <StyledLayout>
         <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
             <Menu.Item key="1">
@@ -35,11 +44,11 @@ class App extends Component {
 
         <Layout>
           <Content style={{margin: '0 16px'}}>
-            <div style={{padding: 24, background: '#fff', minHeight: 360}}>Bill is a cat.</div>
+
           </Content>
-          <Footer style={{textAlign: 'center'}}>Smena school © 2017</Footer>
+          <StyledFooter>Smena school © 2017</StyledFooter>
         </Layout>
-      </Layout>
+      </StyledLayout>
     )
   }
 }
