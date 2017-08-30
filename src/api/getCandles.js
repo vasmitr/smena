adbf0c99bfd9253226761b46b86411021da2b629
Vasily <vm@smena.space>
2017-08-30 17:13:23 +0500

    График для эфира

--- src/api/getCandles.js
+++ src/api/getCandles.js
@@ -1,16 +1,25 @@
-import axios from 'axios'
+import axios from "axios";
 
-const ONE_MONTH = 2592000000
-const API_URL = 'https://api.bitfinex.com/v2'
+const ONE_MONTH = 2592000000;
+const API_URL = "https://api.bitfinex.com/v2";
 
-export default async function() {
-  const urlLastTrade = API_URL + '/candles/trade:1m:tBTCUSD/last'
+export default async function(coin) {
+  const urlLastTrade = API_URL + "/candles/trade:1m:tBTCUSD/last";
 
-  const [MTS] = await axios.get(urlLastTrade).then(({data}) => data)
+  const [MTS] = await axios.get(urlLastTrade).then(({ data }) => data);
+  var candlesDataUrl;
 
-  const candlesDataUrl =
-    API_URL + `/candles/trade:1m:tBTCUSD/hist?start=${MTS - ONE_MONTH}&end=${MTS}&limit=1000`
-
-  const data = await axios.get(candlesDataUrl).then(({data}) => data)
-  return data
+  if (coin === "BTC") {
+    candlesDataUrl =
+      API_URL +
+      `/candles/trade:1m:tBTCUSD/hist?start=${MTS -
+        ONE_MONTH}&end=${MTS}&limit=1000`;
+  } else if (coin === "ETH") {
+    candlesDataUrl =
+      API_URL +
+      `/candles/trade:1m:tETHUSD/hist?start=${MTS -
+        ONE_MONTH}&end=${MTS}&limit=1000`;
+  }
+  const data = await axios.get(candlesDataUrl).then(({ data }) => data);
+  return data;
 }
